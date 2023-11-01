1d6d9133c8af55c32b2d79bbb6f570568890b007
Sagar Rana <sagar@solutionsquares.com>
2023-11-01 15:15:29 +0530

    Code Change for production

--- pm2.config.js
+++ pm2.config.js
@@ -1,17 +1,16 @@
-/* eslint-disable max-len */
 module.exports = {
-	apps: [
-		{
-			name: 'monarc-backend',
-			script: 'NODE_ENV=production node --experimental-loader newrelic/esm-loader.mjs -r newrelic dist/main.js',
-			instances: 'max',
-			exec_mode: 'cluster',
-			autorestart: true,
-			watch: false,
-			max_memory_restart: '1G',
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-	],
+  apps: [
+    {
+      name: 'monarc-backend',
+      script: 'node --experimental-loader newrelic/esm-loader.mjs -r newrelic ./dist/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
